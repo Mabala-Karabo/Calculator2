4110ea9e9721808dd83416b18142d2c785c6da79
Mabala-Karabo <mabalakarabo25@gmail.com>
2023-09-18 16:01:06 +0200

    Add project files.

---
+++ Calculator2/Form1.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        txtNum1 = New TextBox()
+        txtOperator = New TextBox()
+        txtNum2 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(595, 288)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 0
+        Button1.Text = "Calculate"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(73, 48)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(103, 25)
+        Label1.TabIndex = 1
+        Label1.Text = "Enter num1"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(73, 105)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(129, 25)
+        Label2.TabIndex = 2
+        Label2.Text = "Enter Operator"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(73, 173)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(103, 25)
+        Label3.TabIndex = 3
+        Label3.Text = "Enter num1"
+        ' 
+        ' txtNum1
+        ' 
+        txtNum1.Location = New Point(182, 42)
+        txtNum1.Name = "txtNum1"
+        txtNum1.Size = New Size(150, 31)
+        txtNum1.TabIndex = 4
+        ' 
+        ' txtOperator
+        ' 
+        txtOperator.Location = New Point(198, 105)
+        txtOperator.Name = "txtOperator"
+        txtOperator.Size = New Size(150, 31)
+        txtOperator.TabIndex = 5
+        ' 
+        ' txtNum2
+        ' 
+        txtNum2.Location = New Point(189, 170)
+        txtNum2.Name = "txtNum2"
+        txtNum2.Size = New Size(150, 31)
+        txtNum2.TabIndex = 6
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtNum2)
+        Controls.Add(txtOperator)
+        Controls.Add(txtNum1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtNum1 As TextBox
+    Friend WithEvents txtOperator As TextBox
+    Friend WithEvents txtNum2 As TextBox
+End Class
---
+++ Calculator2/Form1.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Diagnostics.Eventing.Reader
+
+Public Class Form1
+    Dim number1 As Double
+    Dim number2 As Double
+    Dim operator1 As String
+    Dim number3 As Double
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'number1 = txtNum1.Text
+        operator1 = txtOperator.Text
+        'number2 = txtNum2.Text
+        If IsNumeric(txtNum1.Text) Then
+            number1 = CInt(txtNum1.Text)
+        Else
+            MsgBox("number1 is not numeric")
+        End If
+        If IsNumeric(txtNum2.Text) Then
+            number2 = CInt(txtNum2.Text)
+        Else
+            MsgBox("number1 is not numeric")
+        End If
+        If operator1 = "*" Then
+            number3 = number1 * number2
+            MsgBox("the product of " & number1 & " and " & number2 & " is " & number3)
+        ElseIf (operator1 = "/" Or operator1 = "\") Then
+            number3 = number1 \ number2
+            MsgBox("the quotient of " & number1 & " and " & number2 & " is " & number3)
+        ElseIf operator1 = "+" Then
+            number3 = number1 + number2
+            MsgBox("the sum of " & number1 & " and " & number2 & " is " & number3)
+        ElseIf operator1 = "-" Then
+            number3 = number1 - number2
+            MsgBox("the difference between " & number1 & " and " & number2 & " is " & number3)
+        ElseIf operator1 = "%" Then
+            number3 = number1 Mod number2
+            MsgBox("the remainder of " & number1 & " and " & number2 & " is " & number3)
+        Else
+            MsgBox("you operator is invalid or not specified")
+        End If
+    End Sub
+End Class
